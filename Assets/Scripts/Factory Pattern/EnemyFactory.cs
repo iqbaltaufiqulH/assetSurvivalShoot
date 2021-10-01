c2998311fba8287ec2af6a3b31c252e24e0dca6b
iqbaltaufiqulH <fisttaste@gmail.com>
2021-10-01 22:23:33 +0700

    3D game

---
+++ Assets/Scripts/Factory Pattern/EnemyFactory.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System;
+
+public class EnemyFactory : MonoBehaviour, IFactory
+{
+    [SerializeField]
+    public GameObject[] enemyPrefab;
+
+    public GameObject FactoryMethod(int tag)
+    {
+        GameObject enemy = Instantiate(enemyPrefab[tag]);
+        return enemy;
+    }
+}
---
+++ Assets/Scripts/Factory Pattern/IFactory.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System;
+
+public interface IFactory
+{
+    GameObject FactoryMethod(int tag);
+}
